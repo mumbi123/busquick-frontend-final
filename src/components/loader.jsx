22f102f045379bca74a2dea9ef4beaaf3cfb1396
moses lesa <94636519+mumbi123@users.noreply.github.com>
2025-10-04 19:47:11 +0800

    Update loader.jsx

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 function Loader() {
-    return (
-        <div className='spinner-parent'>
-            <div className="spinner-border" role="status">
-            </div>
-        </div>
-    );
+  return (
+    <div className='spinner-parent'>
+      <div 
+        className="spinner-border" 
+        role="status" 
+        aria-hidden="true"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
 }
 
 export default Loader;
